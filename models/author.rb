02bcd71a4b86bf9d44f117376f887aefde4bd05e
Nikita Schultz <nikita.schultz1996@gmail.com>
2020-05-29 11:39:49 +0100

    added author class

--- models/author.rb
+++ models/author.rb
@@ -1,22 +1,18 @@
-require('minitest/autorun')
-require('minitest/reporters')
-require_relative('../Author')
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
-class TestAuthor < MiniTest::Test
-  def setup()
-    @author1 = Author.new("first_name" => "Patrick", "last_name" => "Rothfuss")
-    @author2 = Author.new("first_name" => "Markus", "last_name" => "Zusak")
+require_relative('../db/sql_runner')
+
+class Author
+
+  attr_reader :id
+  attr_accessor :first_name, :last_name
+
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @first_name = options["first_name"]
+    @last_name = options["last_name"]
   end
 
-  def test_initialize_new_author
-    assert_equal("Patrick", @author1.first_name)
-    assert_equal("Rothfuss", @author1.last_name)
-    assert_equal("Markus", @author1.first_name)
-    assert_equal("Zusak", @author1.last_name)
+  def full_name
+    return "#{@first_name} #{@last_name}"
   end
 
-  def test_print_full_name
-    assert_equal("Patrick Rothfuss", @author1.full_name)
-    assert_equal("Markus Zusak", @author1.full_name)
-  end
 end
