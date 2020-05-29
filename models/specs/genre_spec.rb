e192dbd3b183888aa040e56e5e10ab672a0fcbad
Nikita Schultz <nikita.schultz1996@gmail.com>
2020-05-29 11:04:43 +0100

    added files for models and set up genre spec file

---
+++ models/specs/genre_spec.rb
@@ -0,0 +1,15 @@
+require('minitest/autorun')
+require('minitest/reporters')
+require_relative('../Genre')
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+class TestGenre < MiniTest::Test
+  def setup()
+    @genre1 = Genre.new({"name" => "Crime"})
+    @genre2 = Genre.new({"name" => "Sci-Fi"})
+  end
+
+  def test_initialize_new_genre
+    assert_equal("Crime", @genre1.name())
+    assert_equal("Sci-Fi", @genre2.name())
+  end
+end
