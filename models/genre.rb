eb3c7272931f22786fbd8928982bda59b3909ec1
Nikita Schultz <nikita.schultz1996@gmail.com>
2020-05-29 11:28:44 +0100

    crud functions for genre class

--- models/genre.rb
+++ models/genre.rb
@@ -11,23 +11,34 @@ class Genre
   end
 
   def Genre.all()
-
+    sql = "SELECT * FROM genres;"
+    pg_result = SqlRunner.run(sql)
+    return pg_result.map{|genre_info| Genre.new(genre_info)}
   end
 
-  def Genre.find()
-
+  def Genre.find(id)
+    sql = "SELECT * FROM genres WHERE id = $1"
+    values = [id]
+    pg_result = SqlRunner.run(sql, values)
+    return Genre.new(pg_result[0])
   end
 
   def save()
-
+    sql = "INSERT INTO genres (name) VALUES ($1) RETURNING *"
+    values = [@name]
+    SqlRunner.run(sql, values)
   end
 
   def delete()
-
+    sql = "DELETE FROM genres WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
   end
 
   def update()
-
+    sql = "UPDATE genres SET name = $1 WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run(sql, values)
   end
 
 end
--- models/specs/genre_spec.rb
+++ models/specs/genre_spec.rb
@@ -1,7 +1,9 @@
 require('minitest/autorun')
 require('minitest/reporters')
 require_relative('../Genre')
+
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
 class TestGenre < MiniTest::Test
   def setup()
     @genre1 = Genre.new({"name" => "Crime"})
@@ -13,30 +15,4 @@ class TestGenre < MiniTest::Test
     assert_equal("Sci-Fi", @genre2.name())
   end
 
-  def test_save_genre
-    @genre1.save()
-    @genre2.save()
-    assert_not_nil(@genre1.id())
-    assert_not_nil(@genre2.id())
-  end
-
-  def test_genre_all
-    @genre1.save()
-    @genre2.save()
-    assert_equal(2, Genre.all().length())
-  end
-
-  def test_delete_genre
-    @genre1.save()
-    @genre2.save()
-    @genre1.delete()
-    assert_equal(1, Genre.all().length())
-  end
-
-  def test_update_genre
-    @genre1.save()
-    @genre1.name = "Fantasy"
-    @genre1.update
-    assert_equal("Fantasy", Genre.find(@genre1.id())
-  end
 end
