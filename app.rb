36656f356c6f7fb5d509f4174677d64a1706b1ea
Nikita Schultz <nikita.schultz1996@gmail.com>
2020-06-01 08:59:26 +0100

    added tags index, new, show pages

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require_relative('./controllers/books_controller')
 require_relative('./controllers/authors_controller')
 require_relative('./controllers/serieses_controller')
 require_relative('./controllers/genres_controller')
+require_relative('./controllers/tags_controller')
+
 
 get '/' do
   erb(:index)
---
+++ controllers/tags_controller.rb
@@ -0,0 +1,30 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('../models/tag')
+also_reload('../models/*')
+
+get '/tags' do
+  @tags = Tag.all()
+  erb(:"tags/index")
+end
+
+get '/tags/new' do
+  erb(:"tags/new")
+end
+
+post '/tags' do
+  tag = Tag.new(params)
+  tag.save()
+  redirect(:"tags/#{tag.id()}")
+end
+
+get '/tags/:id' do
+  @tag = Tag.find(params["id"])
+  erb(:"tags/show")
+end
+
+post '/genres/:id/delete' do
+  @tag = Tag.find(params["id"])
+  @tag.delete()
+  redirect('/tags')
+end
